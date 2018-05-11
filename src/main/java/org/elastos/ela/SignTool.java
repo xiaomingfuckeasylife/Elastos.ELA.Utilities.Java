ef6789ddd413ed72e2920a99a9b22076257b8d00
xiaomingfuckeasylife <1406629722@qq.com>
2018-05-11 15:05:35 +0800

    finish verify sign

--- src/main/java/org/elastos/ela/SignTool.java
+++ src/main/java/org/elastos/ela/SignTool.java
@@ -1,16 +1,16 @@
 package org.elastos.ela;
 
-import org.elastos.ela.bitcoinj.Sha256Hash;
-import org.elastos.ela.bitcoinj.Utils;
-import org.spongycastle.crypto.digests.SHA256Digest;
-import org.spongycastle.crypto.params.ECPrivateKeyParameters;
-import org.spongycastle.crypto.signers.ECDSASigner;
-import org.spongycastle.crypto.signers.HMacDSAKCalculator;
-import org.spongycastle.crypto.signers.RandomDSAKCalculator;
-
 import java.math.BigInteger;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+
+import org.elastos.ela.bitcoinj.Sha256Hash;
+import org.elastos.ela.bitcoinj.Utils;
+import org.spongycastle.asn1.sec.SECNamedCurves;
+import org.spongycastle.asn1.x9.X9ECParameters;
+import org.spongycastle.crypto.params.ECPrivateKeyParameters;
+import org.spongycastle.crypto.params.ECPublicKeyParameters;
+import org.spongycastle.crypto.signers.ECDSASigner;
+import org.spongycastle.crypto.signers.RandomDSAKCalculator;
 
 /**
  * Created by nan on 18/1/14.
@@ -40,6 +40,28 @@ public class SignTool {
     }
 
     public static boolean verify(byte[] msg,byte[] sig,byte[] pub){
-        return false;
+    	if(sig.length != 64 ) {
+    		return false;
+    	}
+    	byte rb[] = new byte[sig.length/2];
+    	byte sb[] = new byte[sig.length/2];
+    	System.arraycopy(sig, 0, rb, 0, rb.length);
+    	System.arraycopy(sig, sb.length, sb, 0, sb.length);
+    	BigInteger r = parseBigIntegerPositive(new BigInteger(rb),rb.length * 8);
+    	BigInteger s =  parseBigIntegerPositive(new BigInteger(sb),rb.length * 8);
+    	
+    	msg = Sha256Hash.hash(msg);
+    	X9ECParameters curve = SECNamedCurves.getByName("secp256r1");
+    	ECDSASigner singer = new ECDSASigner();
+    	ECPublicKeyParameters publicKey = new ECPublicKeyParameters(curve.getCurve().decodePoint(pub),ECKey.CURVE);
+    	singer.init(false, publicKey);
+    	return singer.verifySignature(msg, r, s);
+    }
+    
+    public static BigInteger parseBigIntegerPositive( BigInteger b,int bitlen) {
+        if (b.compareTo(BigInteger.ZERO) < 0)
+            b = b.add(BigInteger.ONE.shiftLeft(bitlen));
+        return b;
     }
 }
+
